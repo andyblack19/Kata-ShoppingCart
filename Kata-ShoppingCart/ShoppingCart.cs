1c44bfebe018fe1da5473371808779f77d2530df
Andy Black <andyblack16@gmail.com>
2017-08-15 16:31:03 +0100

    Supports pricing multiple of same item

--- Kata-ShoppingCart/ShoppingCart.cs
+++ Kata-ShoppingCart/ShoppingCart.cs
@@ -1,14 +1,19 @@
+using System.Collections.Generic;
+
 namespace Kata_ShoppingCart
 {
     public class ShoppingCart
     {
+        private readonly List<string> _items = new List<string>();
+
         public void Scan(string sku)
         {
+            _items.Add(sku);
         }
 
         public int Total()
         {
-            return 50;
+            return _items.Count * 50;
         }
     }
 }
--- Kata-ShoppingCart/ShoppingCartTests.cs
+++ Kata-ShoppingCart/ShoppingCartTests.cs
@@ -5,11 +5,20 @@ namespace Kata_ShoppingCart
     public class ShoppingCartTests
     {
         [Test]
-        public void Single_item_in_cart_gives_individual_price()
+        public void Single_item_gives_individual_price()
         {
             var cart = new ShoppingCart();
             cart.Scan("A99");
             Assert.That(cart.Total(), Is.EqualTo(50));
+        }
+
+        [Test]
+        public void Two_of_same_item_cart_gives_double_the_individual_price()
+        {
+            var cart = new ShoppingCart();
+            cart.Scan("A99");
+            cart.Scan("A99");
+            Assert.That(cart.Total(), Is.EqualTo(100));
         }
     }
 }
